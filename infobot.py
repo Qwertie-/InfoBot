d37a56d231f41540df3989a33d148293df9858e0
Luke Picciau <luke.b.picciau@gmail.com>
2015-12-13 17:10:16 +1030

    Added redis import and connection

--- infobot.py
+++ infobot.py
@@ -1,16 +1,15 @@
 from pyrcb import IRCBot
+import redis
 
 
 class InfoBot(IRCBot):
     def on_message(self, message, nickname, channel, is_query):
-        command = message.split(" ")
+        command = message.split(" ") #Split the message
         if command[0] == ".set":
-            self.send(channel, "Set info:" + str(command[1:]))
+            self.send(channel, "Set info: " + str(command[1:]))
 
         if command[0] == ".enable":
             pass
-
-
 
 
 def main():
@@ -19,6 +18,8 @@ def main():
     bot.register("InfoBot")
     bot.join("#channel")
     bot.listen()
+
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 if __name__ == "__main__":
     main()
