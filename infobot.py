c40ad5356f93e110a2558c12b4863598ae1fa461
Luke Picciau <luke.b.picciau@gmail.com>
2015-12-15 00:03:06 +1030

    Data can now be stored and retrieved from redis

--- infobot.py
+++ infobot.py
@@ -4,12 +4,14 @@ import redis
 
 class InfoBot(IRCBot):
     def on_message(self, message, nickname, channel, is_query):
+        r = redis.StrictRedis(host='localhost', port=6379, db=0)
         command = message.split(" ") #Split the message
         if command[0] == ".set":
             self.send(channel, "Set info: " + str(command[1:]))
+            r.set(nickname, str(command[1:]))
 
         if command[0] == ".enable":
-            pass
+            self.send(channel, "Info for " + command[1] + str(r.get(command[1])))
 
 
 def main():
@@ -19,7 +21,6 @@ def main():
     bot.join("#channel")
     bot.listen()
 
-    r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 if __name__ == "__main__":
     main()
