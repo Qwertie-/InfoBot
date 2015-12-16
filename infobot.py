3e5de01cee47d02915d2cb91d7b9e62aa9c2631e
Luke Picciau <luke.b.picciau@gmail.com>
2015-12-16 16:52:02 +1030

    Comments

--- infobot.py
+++ infobot.py
@@ -1,11 +1,12 @@
 from pyrcb import IRCBot
 import redis
 
-
 class InfoBot(IRCBot):
     def on_message(self, message, nickname, channel, is_query):
         r = redis.StrictRedis(host='localhost', port=6379, db=0)
         command = message.split(" ") #Split the message
+
+        #Checks if message was from a PM or channel
         if is_query:
             reply_to = nickname
         else:
@@ -18,7 +19,7 @@ class InfoBot(IRCBot):
         if command[0] == ".enable":
             self.send(reply_to, "Info for " + command[1] + str(r.get(command[1])))
 
-def main():
+def main():  #TODO: Move config to its own file
     bot = InfoBot()
     bot.connect("host", 6667)
     bot.register("InfoBot")
