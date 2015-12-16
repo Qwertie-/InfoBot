f7a78ee6e6038767bbd056809b4b3ee5e0187db4
Luke Picciau <luke.b.picciau@gmail.com>
2015-12-16 16:46:39 +1030

    Handles PMs

--- infobot.py
+++ infobot.py
@@ -6,13 +6,20 @@ class InfoBot(IRCBot):
     def on_message(self, message, nickname, channel, is_query):
         r = redis.StrictRedis(host='localhost', port=6379, db=0)
         command = message.split(" ") #Split the message
-        if command[0] == ".set":
-            self.send(channel, "Set info: " + str(command[1:]))
-            r.set(nickname, str(command[1:]))
+        if is_query:
+            if command[0] == ".set":
+                self.send(nickname, "Set info: " + str(command[1:]))
+                r.set(nickname, str(command[1:]))
 
-        if command[0] == ".enable":
-            self.send(channel, "Info for " + command[1] + str(r.get(command[1])))
+            if command[0] == ".enable":
+                self.send(nickname, "Info for " + command[1] + str(r.get(command[1])))
+        else:
+            if command[0] == ".set":
+                self.send(channel, "Set info: " + str(command[1:]))
+                r.set(nickname, str(command[1:]))
 
+            if command[0] == ".enable":
+                self.send(channel, "Info for " + command[1] + str(r.get(command[1])))            
 
 def main():
     bot = InfoBot()
