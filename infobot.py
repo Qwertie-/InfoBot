29a95c89e7e052b7b633270dfc048c70f20dd83d
Luke Picciau <luke.b.picciau@gmail.com>
2016-01-05 20:59:08 +1030

    Started work on checking whois to see if user is logged in

--- infobot.py
+++ infobot.py
@@ -13,6 +13,9 @@ class InfoBot(IRCBot):
             reply_to = channel
 
         if command[0] == ".add":
+            #Send server a whois to check that a user is authenticated with NickServ
+            self.send_raw("whois " + nickname)
+
             if len(command) >= 2:
                 self.send(reply_to, "Set info: " + ' '.join(command[1:]))
                 r.set(nickname.lower(), ' '.join(command[1:]))
@@ -24,6 +27,11 @@ class InfoBot(IRCBot):
                 self.send(reply_to, "Info for " + command[1] + ": " + r.get(command[1].lower()).decode('utf-8'))
             else:
                 self.send(reply_to, "Usage '.info username'")
+
+    #TODO: Check for whois from server
+    def on_raw(self, nickname, command, args):
+        pass
+
 
 def main():  #TODO: Move config to its own file
     bot = InfoBot()
