ca56560338bad20ce71d0e81e733c71581166ef1
Martijn Arts <arts.martijn@gmail.com>
2016-01-05 09:04:46 -0500

    Keep the redis connection alive

--- infobot.py
+++ infobot.py
@@ -4,11 +4,15 @@ import redis
 import config
 
 class InfoBot(IRCBot):
+    def __init__(self, *args, **kwargs):
+        self.r = redis.StrictRedis(host=config.REDIS_HOST,
+                                   port=config.REDIS_PORT,
+                                   db=config.REDIS_DB,
+                                   password=config.REDIS_PASS)
+
+        super().__init__(*args, **kwargs)
+
     def on_message(self, message, nickname, channel, is_query):
-        r = redis.StrictRedis(host=config.REDIS_HOST,
-                              port=config.REDIS_PORT,
-                              db=config.REDIS_DB,
-                              password=config.REDIS_PASS)
         command = message.split(" ") #Split the message
 
         #Checks if message was from a PM or channel
@@ -23,13 +27,13 @@ class InfoBot(IRCBot):
 
             if len(command) >= 2:
                 self.send(reply_to, "Set info: " + ' '.join(command[1:]))
-                r.set(nickname.lower(), ' '.join(command[1:]))
+                self.r.set(nickname.lower(), ' '.join(command[1:]))
             else:
                 self.send(reply_to, "Usage: '.add some info about yourself here'")
 
         if command[0] == ".info":
             if len(command) >= 2:
-                info = r.get(command[1].lower())
+                info = self.r.get(command[1].lower())
                 if info is None:
                     self.send(reply_to, "No info found for " + command[1] + ".")
                     return
