2c7027c931d7837947675ffe34b45df80151f742
Jacob Edelman <edelman.jd@gmail.com>
2016-01-08 08:03:18 -0500

    refactored, adding parsing

--- infobot.py
+++ infobot.py
@@ -1,5 +1,6 @@
 from pyrcb import IRCBot
 import redis
+from parse import parse
 
 import config
 
@@ -12,34 +13,41 @@ class InfoBot(IRCBot):
 
         super().__init__(*args, **kwargs)
 
+    def add_info(self, nick, channel, reply_to, info):
+        #Send server a whois to check that a user is authenticated with NickServ
+        self.send_raw("whois " + nickname)
+
+        if info: # aka, info != ""
+            self.send(reply_to, "Set info: " + info)
+            self.r.set(nickname.lower(), info)
+        else:
+            self.send(reply_to, "Usage: '.add some info about yourself here'")
+    
+    def get_info(self, nick, channel, reply_to, name):
+        if name: #aka, name != ""
+            info = self.r.get(name.lower())
+            if info is None:
+                self.send(reply_to, "No info found for " + name + ".")
+                return
+            self.send(reply_to, name + ": " + info.decode('utf-8'))
+        else:
+            self.send(reply_to, "Usage: '.info username'")
+
     def on_message(self, message, nickname, channel, is_query):
-        command = message.split(" ") #Split the message
 
         #Checks if message was from a PM or channel
         if is_query:
             reply_to = nickname
         else:
             reply_to = channel
-
-        if command[0] == ".add":
-            #Send server a whois to check that a user is authenticated with NickServ
-            self.send_raw("whois " + nickname)
-
-            if len(command) >= 2:
-                self.send(reply_to, "Set info: " + ' '.join(command[1:]))
-                self.r.set(nickname.lower(), ' '.join(command[1:]))
-            else:
-                self.send(reply_to, "Usage: '.add some info about yourself here'")
-
-        if command[0] == ".info":
-            if len(command) >= 2:
-                info = self.r.get(command[1].lower())
-                if info is None:
-                    self.send(reply_to, "No info found for " + command[1] + ".")
-                    return
-                self.send(reply_to, command[1] + ": " + info.decode('utf-8'))
-            else:
-                self.send(reply_to, "Usage: '.info username'")
+        commands = [
+                    (parse(".add {info}"), self.add_info), 
+                    (parse(".info {name}"), self.get_info)
+                   ]
+        for grammar, func in commands:
+            attempt = parse(i, message)
+            if attempt != None:
+                return commands[attempt](nick, channel, reply_to, **attempt)
 
     #TODO: Check for whois from server
     def on_raw(self, nickname, command, args):
