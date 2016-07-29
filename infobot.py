0e520d7a8d9c05e31f29d77b6534c4cde85f415c
Jacob Edelman <edelman.jd@gmail.com>
2016-07-29 15:39:11 -0400

    Added deleting, freezing, and unfreezing.

--- infobot.py
+++ infobot.py
@@ -14,18 +14,56 @@ class InfoBot(IRCBot):
         super().__init__(*args, **kwargs)
 
     def add_info(self, nickname, channel, reply_to, info):
-        #Send server a whois to check that a user is authenticated with NickServ
-        self.send_raw("whois " + nickname)
-        self.send(reply_to, "Set info: " + info)
-        self.r.set(nickname.lower(), info)
+        #Send server a whois to check that a user is authenticated with NickServ # Whois not doing anything??
+        # self.send_raw("whois " + nickname)
+        #Assumes * is not allowed at the start of nicknames
+        info_meta = self.r.get("*" + nickname.lower())
+        if info_meta != None: info_meta = info_meta.decode("utf-8")
+        if info_meta == "frozen":
+            if self.nicklist[channel][nickname].is_op:
+                self.r.set(nickname.lower(), info)
+                self.send(reply_to, "Set info: " + info)
+            else:
+                self.send(reply_to, "Only mods can do this.")
+        else:
+            self.r.set(nickname.lower(), info)
+            self.send(reply_to, "Set info: " + info)
 
     def get_info(self, nickname, channel, reply_to, name_raw):
         name = name_raw.strip()
-        info = self.r.get(name.lower())
-        if info is None:
+        if name.startswith("*"):
             self.send(reply_to, "No info found for " + name + ".")
-            return
-        self.send(reply_to, name + ": " + info.decode('utf-8'))
+        else:
+            info = self.r.get(name.lower())
+            if info is None:
+                self.send(reply_to, "No info found for " + name + ".")
+            else:
+                self.send(reply_to, name + ": " + info.decode('utf-8'))
+
+    def delete_info(self, nickname, channel, reply_to, name_raw):
+        name = name_raw.strip()
+        if self.nicklist[channel][nickname].is_op:
+            self.send(reply_to, "Deleted info for " + name + ".")
+            self.r.delete(name.lower()) # does not delete freezing!!
+        else:
+            self.send(reply_to, "Only mods can do this.")
+
+    def freeze_info(self, nickname, channel, reply_to, name_raw):
+        name = name_raw.strip()
+        if self.nicklist[channel][nickname].is_op:
+            self.send(reply_to, "Froze info for " + name + ".")
+            self.r.set("*" + name.lower(), "frozen")
+        else:
+            self.send(reply_to, "Only mods can do this.")
+
+    def unfreeze_info(self, nickname, channel, reply_to, name_raw):
+        name = name_raw.strip()
+        if self.nicklist[channel][nickname].is_op:
+            self.send(reply_to, "Unfroze info for " + name + ".")
+            self.r.set("*" + name.lower(), "")
+        else:
+            self.send(reply_to, "Only mods can do this.")
+
 
     def prompt(self, input_form, output_form):
         def output_func(nick, chan, reply_to, **args):
@@ -42,8 +80,14 @@ class InfoBot(IRCBot):
         commands = [
                     (parse.compile(".add {info}"), self.add_info),
                     (parse.compile(".info {name_raw}"), self.get_info),
+                    (parse.compile(".delete {name_raw}"), self.delete_info),
+                    (parse.compile(".freeze {name_raw}"), self.freeze_info),
+                    (parse.compile(".unfreeze {name_raw}"), self.unfreeze_info),
                     self.prompt(".add", "Usage: '.add some info about yourself here'"),
-                    self.prompt(".info", "Usage: '.info username'")
+                    self.prompt(".info", "Usage: '.info username'"),
+                    self.prompt(".delete", "Mod only command"),
+                    self.prompt(".freeze", "Mod only command"),
+                    self.prompt(".unfreeze", "Mod only command")
                    ]
         for parser, func in commands:
             attempt = parser.parse(message.strip()) #stripping the message here
